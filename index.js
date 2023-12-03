1cc13777c57e6693cd3eec3b7d4aec7d671d0a1d
cerramaximiliano <cerramaximiliano@gmail.com>
2023-12-03 15:53:15 -0300

    first commit

--- index.js
+++ index.js
@@ -2,14 +2,66 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  description: String,
+  duration: Number,
+  date: Date
+});
+
+const User = mongoose.model("User", userSchema);
+
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', async (req, res) => {
+    const {username} = req.body;  
+    const user = new User ( {username} );
+    try {
+      const newUser = await user.save();
+      res.json({
+        newUser
+      })
+    }catch(err){
+      console.log(err)
+    }
+});
 
+// {
+//   username: "fcc_test",
+//   description: "test",
+//   duration: 60,
+//   date: "Mon Jan 01 1990",
+//   _id: "5fb5853f734231456ccb3b05"
+// }
+
+// {
+//   username: "fcc_test",
+//   _id: "5fb5853f734231456ccb3b05"
+// }
+
+// {
+//   username: "fcc_test",
+//   count: 1,
+//   _id: "5fb5853f734231456ccb3b05",
+//   log: [{
+//     description: "test",
+//     duration: 60,
+//     date: "Mon Jan 01 1990",
+//   }]
+// }
 
 
 
